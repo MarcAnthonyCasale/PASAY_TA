5dbead0f7673a07873b2ccdc759228e28a1c6cd6
Anthony <marcanthonycasale1@gmail.com>
2021-06-14 11:32:57 +0800

    Controllers

--- app/Models/realtimev.php
+++ app/Models/realtimev.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class realtimev extends Model
+class Pealtimev extends Model
 {
     use HasFactory;
 }
