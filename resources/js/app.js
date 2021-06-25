6532745e1dad8688a95adc9326a02cee506aaf98
Marc Anthony <Marcanthonycasale1@gmail.com>
2021-06-25 10:00:30 +0800

    push

--- resources/js/app.js
+++ resources/js/app.js
@@ -6,10 +6,6 @@
 
 require('./bootstrap');
 
-import vuetify from './plugins/vuetify'
-import Vue from 'vue'
-import router from  "./router"
-
 window.Vue = require('vue').default;
 
 /**
@@ -23,6 +19,7 @@ window.Vue = require('vue').default;
 // const files = require.context('./', true, /\.vue$/i)
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
 
+Vue.component('example-component', require('./components/ExampleComponent.vue').default);
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
@@ -31,7 +28,5 @@ window.Vue = require('vue').default;
  */
 
 const app = new Vue({
-    vuetify,
-    router,
     el: '#app',
 });
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,26 +7,15 @@
             <div class="card">
                 <div class="card-header">{{ __('Dashboard') }}</div>
 
-                <div>
-                    <b-navbar type="dark" variant="dark">
-                      <b-navbar-nav>
-                        <b-nav-item href="#">Home</b-nav-item>
-                  
-                        <!-- Navbar dropdowns -->
-                        <b-nav-item-dropdown text="Lang" right>
-                          <b-dropdown-item href="#">EN</b-dropdown-item>
-                          <b-dropdown-item href="#">ES</b-dropdown-item>
-                          <b-dropdown-item href="#">RU</b-dropdown-item>
-                          <b-dropdown-item href="#">FA</b-dropdown-item>
-                        </b-nav-item-dropdown>
-                  
-                        <b-nav-item-dropdown text="User" right>
-                          <b-dropdown-item href="#">Account</b-dropdown-item>
-                          <b-dropdown-item href="#">Settings</b-dropdown-item>
-                        </b-nav-item-dropdown>
-                      </b-navbar-nav>
-                    </b-navbar>
-                  </div>
+                <div class="card-body">
+                    @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+
+                    {{ __('You are logged in!') }}
+                </div>
             </div>
         </div>
     </div>
